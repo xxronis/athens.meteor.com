84ea4ac4fb149b319c24edbdfef3f276192ea619
Pan Chronopoulos <xxronis@gmail.com>
2014-11-27 22:17:14 +0200

    seo Athens

--- seo.js
+++ seo.js
@@ -1,9 +1,9 @@
 Meteor.startup(function() {
   if(Meteor.isClient) {
     SEO.config({
-      title: 'Meteor Vegas',
+      title: 'Meteor Athens, Greece',
       meta: {
-        'description': 'A Bi-Monthly Meteor.js Meetup in Las Vegas, NV'
+        'description': 'A Bi-Monthly Meteor.js Meetup in Athens, Greece'
       },
       og: {
         'image': Meteor.absoluteUrl('share-image.png')
